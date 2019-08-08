c69436a8443ac834e2267c592bdd2a58beb5d097
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-08-08 15:02:53 -0700

    env variables

--- server/index.js
+++ server/index.js
@@ -2,31 +2,35 @@ const mongoose = require('mongoose');
 const Todo = require('./models/todo');
 
 mongoose.connect(
-  `mongodb+srv://test123:OPUouUSCSphHKZAA@graphql-cluster-omvn0.mongodb.net/todo-list?retryWrites=true&w=majority`,
+  `mongodb+srv://${process.env.MONGO_USER}:${
+    process.env.MONGO_PASSWORD
+  }@graphql-cluster-omvn0.mongodb.net/${
+    process.env.MONGO_DB
+  }?retryWrites=true&w=majority`,
   {useNewUrlParser: true},
 );
 
-// const addTodo = async () => {
-//   const todo = {
-//     text: 'Lawn',
-//     completed: false,
-//   };
+const addTodo = async () => {
+  const todo = {
+    text: 'Roof',
+    completed: false,
+  };
 
-//   const response = await Todo.create(todo);
-//   console.log(response);
-// };
-
-const updateTodo = async () => {
-  const response = await Todo.update(
-    {_id: '5d4c94dd29b83d6aca775be7'},
-    {
-      $set: {
-        text: 'LAWN LAWN',
-      },
-    },
-  );
+  const response = await Todo.create(todo);
   console.log(response);
 };
 
-// addTodo();
-updateTodo();
+// const updateTodo = async () => {
+//   const response = await Todo.update(
+//     {_id: '5d4c94dd29b83d6aca775be7'},
+//     {
+//       $set: {
+//         text: 'LAWN LAWN',
+//       },
+//     },
+//   );
+//   console.log(response);
+// };
+
+addTodo();
+// updateTodo();
