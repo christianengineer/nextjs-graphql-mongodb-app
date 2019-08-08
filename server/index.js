ebd87ac16f47e442ae6c28ee34517c9fbc589036
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-08-08 14:33:02 -0700

    mongoose working

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose');
-const Todo = require('./models/todo')
+const Todo = require('./models/todo');
 
 mongoose.connect(
-  `mongodb+srv://chris.ipanaque@gmail.com:THf8.MChcw-5sdX@graphql-cluster-omvn0.mongodb.net/test?retryWrites=true&w=majority`,
-  {useMongoClient: true},
+  `mongodb+srv://test123:OPUouUSCSphHKZAA@graphql-cluster-omvn0.mongodb.net/todo-list?retryWrites=true&w=majority`,
+  {useNewUrlParser: true},
 );
 
+const addTodo = async () => {
+  const todo = {
+    text: 'Lawn',
+    completed: false,
+  };
+
+  const response = await Todo.create(todo);
+  console.log(response);
+};
+
+addTodo();
