9d06fb694f7c3919cf609c839b8fe5e3b8cda08c
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-08-09 14:23:23 -0700

    add to moved to todolist

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 import {gql} from 'apollo-boost';
 import {useQuery, useMutation} from '@apollo/react-hooks';
@@ -15,6 +17,16 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.paper,
   },
 }));
+
+const ADD_TODO = gql`
+  mutation AddTodo($text: String!) {
+    addTodo(text: $text) {
+      _id
+      text
+      completed
+    }
+  }
+`;
 
 const UPDATE_TODO = gql`
   mutation CompleteTodo($todoId: ID!, $completed: Boolean!) {
@@ -28,11 +40,30 @@ const UPDATE_TODO = gql`
 
 export default function TodoList() {
   const classes = useStyles();
+  const [addTodo] = useMutation(ADD_TODO);
   const [completeTodo] = useMutation(UPDATE_TODO);
+  const [inputs, setInputs] = useState({
+    text: '',
+  });
+
+  const handleAddTodo = (event) => {
+    console.log('addtodo');
+    event.preventDefault();
+    addTodo({variables: {text: inputs.text}});
+    setInputs((inputs) => ({
+      text: '',
+    }));
+  };
 
   const handleToggle = (_id, completedArg) => () => {
-    console.log(_id);
     completeTodo({variables: {todoId: _id, completed: !completedArg}});
+  };
+
+  const handleInputs = (event) => {
+    event.persist();
+    setInputs((inputs) => ({
+      [event.target.id]: event.target.value,
+    }));
   };
 
   const {loading, error, data} = useQuery(gql`
@@ -49,23 +80,44 @@ export default function TodoList() {
   if (error) return <p>Error: :(</p>;
 
   return (
-    <List className={classes.root}>
-      {data.todos.map(({_id, text, completed}) => {
-        return (
-          <ListItem
-            key={_id}
-            role={undefined}
-            dense
-            button
-            onClick={handleToggle(_id, completed)}
-          >
-            <ListItemText id={_id} primary={text} />
-            <ListItemIcon>
-              <Checkbox checked={completed} />
-            </ListItemIcon>
-          </ListItem>
-        );
-      })}
-    </List>
+    <div>
+      <form onSubmit={handleAddTodo}>
+        <TextField
+          id='text'
+          value={inputs.text}
+          label='+ Add todo'
+          margin='normal'
+          fullWidth
+          variant='outlined'
+          onChange={handleInputs}
+        />
+        <Button
+          variant='contained'
+          color='primary'
+          fullWidth
+          onClick={handleAddTodo}
+        >
+          Submit
+        </Button>
+      </form>
+      <List className={classes.root}>
+        {data.todos.map(({_id, text, completed}) => {
+          return (
+            <ListItem
+              key={_id}
+              role={undefined}
+              dense
+              button
+              onClick={handleToggle(_id, completed)}
+            >
+              <ListItemText id={_id} primary={text} />
+              <ListItemIcon>
+                <Checkbox checked={completed} />
+              </ListItemIcon>
+            </ListItem>
+          );
+        })}
+      </List>
+    </div>
   );
 }
