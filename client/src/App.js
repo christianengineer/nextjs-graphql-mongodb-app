769c1693575a90ff1ba4de89c8cfee0fb2ed590e
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-08-09 12:19:40 -0700

    removed css files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className='App'>
+    <div>
       <TodoList />
     </div>
   );
