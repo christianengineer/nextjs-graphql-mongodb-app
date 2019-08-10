1e269cd9e5f6c2d26e51908c2a9dd2a1527da2fa
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-08-09 18:01:50 -0700

    title

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 import TodoList from './components/TodoList';
 import Container from '@material-ui/core/Container';
 
 function App() {
   return (
     <Container maxWidth='sm'>
+      <Typography variant='h3' align='center' gutterBottom>
+        GraphQL Todo List!
+      </Typography>
       <TodoList />
     </Container>
   );
