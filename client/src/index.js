38451eadad1061ecacaba0ba13c0ba2c66ce1228
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-08-09 18:21:19 -0700

    client server heroku setup

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import {ApolloProvider} from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/api/',
+  uri: '/api',
 });
 
 ReactDOM.render(
