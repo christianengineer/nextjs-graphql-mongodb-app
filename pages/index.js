c40a9e57af152816384b28cd30d70c6fcb52acfb
Christian Ipanaque <christian.ipanaque@formidable.com>
2021-02-16 19:57:21 -0800

    add mongoose connection

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import dbConnect from '../api/dbConnect';
+import Todo from '../api/models/Todo';
+
+export default function TodoApp() {
+
+}
+
+export async function getServerSideProps() {
+  await dbConnect
+
+  
+}
