e904b23a51fcec944ad825e8ec07554b80e83b51
Christian Ipanaque <christian.ipanaque@formidable.com>
2021-02-17 00:26:44 -0800

    update mongo uri 2

--- api/dbConnect.js
+++ api/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 // connect to mongoDB database
 async function dbConnect() {
-  return mongoose.connect(process.env.ENV_MONGODB_URI, {
+  return mongoose.connect(process.env.NEXT_PUBLIC_ENV_MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
