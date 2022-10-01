fb6e90de6264c53d397c1987b4cdb119d67bd6d6
Christian Ipanaque <chris.ipanaque@gmail.com>
2022-10-01 07:09:45 -0700

    add resolvers

---
+++ lib/models/Todo.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+interface Todo {
+  task: string;
+  isCompleted: boolean;
+  created: Date;
+  updated: Date;
+}
+
+const TodoSchema = new mongoose.Schema<Todo>({
+  task: { type: String, required: true },
+  isCompleted: { type: Boolean, required: true },
+  created: { type: Date, required: true, default: Date.now },
+  updated: { type: Date, required: true, default: Date.now },
+});
+
+export default (mongoose.models.Todo as mongoose.Model<Todo>) ||
+  mongoose.model("Todo", TodoSchema);
---
+++ lib/resolvers.ts
@@ -0,0 +1,29 @@
+import dbConnect from './dbConnect';
+import Todo from './models/Todo';
+
+export const resolvers = {
+  Query: {
+    async todos(_parent, _args, _context, _info) {
+      await dbConnect();
+
+      const data = await Todo.find();
+
+      const todos = data.map((doc) => {
+        const todo = doc.toObject();
+        // todo._id = todo._id.toString();
+        // todo.created = todo.created.toString();
+        // todo.updated = todo.updated.toString();
+        return todo;
+      });
+
+      return todos;
+    },
+  },
+  Mutation: {
+    async addTodo(_parent, _args, _context, _info) {
+      await dbConnect();
+      const todo = await Todo.create(_args);
+      return todo;
+    },
+  },
+};
